9ec33ea463fdd4777f88d727e08569f8e752cd30
Musa Sartev <musaatidzhe@gmail.com>
2021-11-11 14:39:42 +0200

    another 6-kyu kata

---
+++ 6-kyu/sortTheOdd.js
@@ -0,0 +1,11 @@
+/* 
+    Task
+    You will be given an array of numbers. You have to sort the odd numbers in 
+    ascending order while leaving the even numbers at their original positions.
+*/
+function sortArray(array) {
+  let odd = array.filter((el) => el % 2 !== 0).sort((prev, next) => prev - next);
+  return array.map((v) => (v % 2 !== 0 ? odd.shift() : v));
+}
+
+console.log(sortArray([5, 3, 2, 8, 1, 4]));
